cb82018267cf4a4061876a6ab5d565d395457b4a
JoaoVictor21BTI <joaovictor.tsouzaz@gmail.com>
2025-11-24 11:48:04 -0300

    concertou o 0 que eu esqueci de apagar, lol

--- Main.java
+++ Main.java
@@ -178,7 +178,7 @@ public class Main {
     public static void ImprimeXMLCancelamentoSAT() {
         if (conexaoAberta) {
 
-            int retorno = ImpressoraDLL.INSTANCE.ImprimeXMLCancelamentoSAT("path=C:\\Users\\victor_tavares\\Desktop\\Java-Aluno EM\\CANC_SAT.xml, 0", "Q5DLkpdRijIRGY6YSSNsTWK1TztHL1vD0V1Jc4spo/CEUqICEb9SFy82ym8EhBRZjbh3btsZhF+sjHqEMR159i4agru9x6KsepK/q0E2e5xlU5cv3m1woYfgHyOkWDNcSdMsS6bBh2Bpq6s89yJ9Q6qh/J8YHi306ce9Tqb/drKvN2XdE5noRSS32TAWuaQEVd7u+TrvXlOQsE3fHR1D5f1saUwQLPSdIv01NF6Ny7jZwjCwv1uNDgGZONJdlTJ6p0ccqnZvuE70aHOI09elpjEO6Cd+orI7XHHrFCwhFhAcbalc+ZfO5b/+vkyAHS6CYVFCDtYR9Hi5qgdk31v23w==", 0);
+            int retorno = ImpressoraDLL.INSTANCE.ImprimeXMLCancelamentoSAT("path=C:\\Users\\victor_tavares\\Desktop\\Java-Aluno EM\\CANC_SAT.xml", "Q5DLkpdRijIRGY6YSSNsTWK1TztHL1vD0V1Jc4spo/CEUqICEb9SFy82ym8EhBRZjbh3btsZhF+sjHqEMR159i4agru9x6KsepK/q0E2e5xlU5cv3m1woYfgHyOkWDNcSdMsS6bBh2Bpq6s89yJ9Q6qh/J8YHi306ce9Tqb/drKvN2XdE5noRSS32TAWuaQEVd7u+TrvXlOQsE3fHR1D5f1saUwQLPSdIv01NF6Ny7jZwjCwv1uNDgGZONJdlTJ6p0ccqnZvuE70aHOI09elpjEO6Cd+orI7XHHrFCwhFhAcbalc+ZfO5b/+vkyAHS6CYVFCDtYR9Hi5qgdk31v23w==", 0);
             if (retorno == 0) {
                 System.out.println("Impressão XMLSAT realizada com sucesso!");
             } else {
@@ -318,4 +318,5 @@ public class Main {
         fis.close();
         return new String(data, StandardCharsets.UTF_8);
     }
+
 }
